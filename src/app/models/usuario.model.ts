d83de3874edae2cecb4ddc72f688efdc048b7f98
hernanmejia <hmejiabenitez@gmail.com>
2021-03-04 15:28:10 -0500

    primer commit

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,13 @@
+
+
+
+export class Usuario{
+
+  constructor(public id: number,
+              public first_name: string,
+              public last_name,
+              public avatar: string){
+
+  }
+}
+
